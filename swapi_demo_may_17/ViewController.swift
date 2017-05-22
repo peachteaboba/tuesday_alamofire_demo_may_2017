3ab18146eb73f96459ebbc3d9a9dd4d9b7cb1cab
Andy Feng <anndyfeng1@gmail.com>
2017-05-22 10:17:58 -0700

    Initial Commit

---
+++ swapi_demo_may_17/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swapi_demo_may_17
+//
+//  Created by Andy Feng on 5/22/17.
+//  Copyright © 2017 Andy Feng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
